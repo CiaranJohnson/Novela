0ada13db6775ff8ce45a6f0a9cc6272af5e7b11f
CiaranJohnson <ciaranmjohnson@aol.com>
2019-02-12 12:25:44 +0000

    Create Personal Information slide

--- app/src/main/java/com/example/novela/LoginActivity.java
+++ app/src/main/java/com/example/novela/LoginActivity.java
@@ -1,27 +1,39 @@
 package com.example.novela;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signInBtn, signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        signInBtn = (Button)findViewById(R.id.signInBtn);
+        signUpBtn = (Button)findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                startActivity(intent);
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                startActivity(intent);
             }
         });
     }
