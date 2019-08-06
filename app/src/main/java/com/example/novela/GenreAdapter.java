821d60de48b7265c7b40a772c693b86160b37a4c
CiaranJohnson <ciaranmjohnson@aol.com>
2019-08-06 10:22:29 +0800

    Genre RecyclerView added

--- app/src/main/java/com/example/novela/GenreAdapter.java
+++ app/src/main/java/com/example/novela/GenreAdapter.java
@@ -1,43 +1,56 @@
 package com.example.novela;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.ViewHolder>{
 
+    private String[] mGenres;
 
-    public GenreAdapter() {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView genre;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            genre = (TextView) itemView.findViewById(R.id.genreTxt);
+        }
+
+    }
+
+    public GenreAdapter(String[] genres) {
+        mGenres = genres;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.genre_list_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // This method gets data based on the item selected from the recycler view
+        holder.genre.setText(mGenres[position]);
+        Log.d(TAG, "onBindViewHolder: " + mGenres[position] + " added.");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mGenres.length;
     }
 
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
 
-            public TextView genre;
-
-            public ViewHolder(View itemView) {
-                super(itemView);
-                genre = (TextView) itemView.findViewById(R.id.genreTxt);
-            }
-
-        }
 }
--- app/src/main/java/com/example/novela/MainActivity.java
+++ app/src/main/java/com/example/novela/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.novela;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.genreRecyclerView);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] genres = {"Adventure", "Horror", "Non-Fiction", "Romance"};
+        mAdapter = new GenreAdapter(genres);
+        recyclerView.setAdapter(mAdapter);
     }
 }
